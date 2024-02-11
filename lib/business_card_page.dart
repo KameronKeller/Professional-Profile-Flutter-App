e9378caf19b4202ee733056e54bc9b31de7576bf
Kameron Keller <mrkameronkeller@gmail.com>
2024-02-11 15:05:59 -0800

    Set up layout

--- lib/business_card_page.dart
+++ lib/business_card_page.dart
@@ -10,11 +10,48 @@ class BusinessCardPage extends StatelessWidget {
       padding: EdgeInsets.all(25.0),
       child: Column(
         children: [
-          CenteredRow(children: [SizedBox(width: 150, height: 150, child: Placeholder())],),
-          CenteredRow(children: [Text("Name")],),
-          CenteredRow(children: [Text("Title")]),
-          CenteredRow(children: [Text("Phone Number")]),
-          CenteredRow(children: [Text("Github"), SizedBox(width: 10.0), Text("Email")]),
+          CenteredRow(children: [SizedBox(width: 200, height: 200, child: Placeholder())],),
+          CenteredRow(
+            children: [
+              Text(
+                "Mr. Anderson",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          CenteredRow(children: [
+            Text(
+              "Full Stack Engineer",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            )
+          ]),
+          CenteredRow(children: [
+            Text(
+              "Phone Number",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            )
+          ]),
+          CenteredRow(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+            Text(
+              "github.com/Rudxain/RGB-digital-rain",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            // SizedBox(width: 10.0),
+            Text(
+              "Email",
+              style: TextStyle(fontSize: 18),)
+          ]),
         ],
       ),
     );
