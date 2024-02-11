3ed7818399979a7315b7472fedce48b3393e1793
Kameron Keller <mrkameronkeller@gmail.com>
2024-02-11 15:05:41 -0800

    Add default value to main axis alignment

--- lib/centered_row.dart
+++ lib/centered_row.dart
@@ -4,15 +4,16 @@ class CenteredRow extends StatelessWidget {
 
   final List<Widget> children;
   final double padding;
+  final MainAxisAlignment mainAxisAlignment;
 
-  const CenteredRow({super.key, required this.children, this.padding = 10.0});
+  const CenteredRow({super.key, required this.children, this.padding = 5.0, this.mainAxisAlignment = MainAxisAlignment.center});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(padding),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: mainAxisAlignment,
         children: children,
       ),
     );
