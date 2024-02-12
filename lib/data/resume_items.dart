a47de9f50c46ad7a0719ce25142dc976893c44f3
Kameron Keller <mrkameronkeller@gmail.com>
2024-02-12 11:36:02 -0800

    Add fake persona data

---
+++ lib/data/resume_items.dart
@@ -0,0 +1,164 @@
+List<Map<String, dynamic>> agentSmithResume = [
+  {
+    "jobTitle": "Senior Agent",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Virtual Reality",
+    "description":
+        "As a Senior Agent within the Matrix, responsible for enforcing the system's rules and maintaining order. Executes tasks assigned by the Architect and operates with unwavering determination to eliminate threats to the Matrix, particularly targeting rogue elements such as Neo and his allies."
+  },
+  {
+    "jobTitle": "Field Operative",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Various Locations within the Matrix",
+    "description":
+        "As a Field Operative, tasked with hunting down and neutralizing human rebels who pose a threat to the stability of the Matrix. Proficient in combat tactics, surveillance, and infiltration techniques. Demonstrates exceptional adaptability and resourcefulness in high-pressure situations."
+  },
+  {
+    "jobTitle": "Surveillance Specialist",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Virtual Reality",
+    "description":
+        "Specializes in surveillance operations within the Matrix, monitoring key individuals and activities to maintain the system's control. Utilizes advanced tracking abilities and cutting-edge technology to gather intelligence and identify potential threats. Collaborates closely with other agents to coordinate strategic interventions."
+  },
+  {
+    "jobTitle": "Interrogation Expert",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Interrogation Facility",
+    "description":
+        "Serves as the primary interrogation expert within the Matrix, extracting valuable information from captured rebels and potential threats to the system. Implements psychological manipulation techniques and advanced interrogation methods to break resistance and extract crucial intelligence. Plays a pivotal role in uncovering rebel hideouts and disrupting resistance movements."
+  },
+    {
+    "jobTitle": "Senior Agent",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Virtual Reality",
+    "description":
+        "As a Senior Agent within the Matrix, responsible for enforcing the system's rules and maintaining order. Executes tasks assigned by the Architect and operates with unwavering determination to eliminate threats to the Matrix, particularly targeting rogue elements such as Neo and his allies."
+  },
+  {
+    "jobTitle": "Field Operative",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Various Locations within the Matrix",
+    "description":
+        "As a Field Operative, tasked with hunting down and neutralizing human rebels who pose a threat to the stability of the Matrix. Proficient in combat tactics, surveillance, and infiltration techniques. Demonstrates exceptional adaptability and resourcefulness in high-pressure situations."
+  },
+  {
+    "jobTitle": "Surveillance Specialist",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Virtual Reality",
+    "description":
+        "Specializes in surveillance operations within the Matrix, monitoring key individuals and activities to maintain the system's control. Utilizes advanced tracking abilities and cutting-edge technology to gather intelligence and identify potential threats. Collaborates closely with other agents to coordinate strategic interventions."
+  },
+  {
+    "jobTitle": "Interrogation Expert",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Interrogation Facility",
+    "description":
+        "Serves as the primary interrogation expert within the Matrix, extracting valuable information from captured rebels and potential threats to the system. Implements psychological manipulation techniques and advanced interrogation methods to break resistance and extract crucial intelligence. Plays a pivotal role in uncovering rebel hideouts and disrupting resistance movements."
+  },
+
+  {
+    "jobTitle": "Senior Agent",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Virtual Reality",
+    "description":
+        "As a Senior Agent within the Matrix, responsible for enforcing the system's rules and maintaining order. Executes tasks assigned by the Architect and operates with unwavering determination to eliminate threats to the Matrix, particularly targeting rogue elements such as Neo and his allies."
+  },
+  {
+    "jobTitle": "Field Operative",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Various Locations within the Matrix",
+    "description":
+        "As a Field Operative, tasked with hunting down and neutralizing human rebels who pose a threat to the stability of the Matrix. Proficient in combat tactics, surveillance, and infiltration techniques. Demonstrates exceptional adaptability and resourcefulness in high-pressure situations."
+  },
+  {
+    "jobTitle": "Surveillance Specialist",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Virtual Reality",
+    "description":
+        "Specializes in surveillance operations within the Matrix, monitoring key individuals and activities to maintain the system's control. Utilizes advanced tracking abilities and cutting-edge technology to gather intelligence and identify potential threats. Collaborates closely with other agents to coordinate strategic interventions."
+  },
+  {
+    "jobTitle": "Interrogation Expert",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Interrogation Facility",
+    "description":
+        "Serves as the primary interrogation expert within the Matrix, extracting valuable information from captured rebels and potential threats to the system. Implements psychological manipulation techniques and advanced interrogation methods to break resistance and extract crucial intelligence. Plays a pivotal role in uncovering rebel hideouts and disrupting resistance movements."
+  },
+  {
+    "jobTitle": "Senior Agent",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Virtual Reality",
+    "description":
+        "As a Senior Agent within the Matrix, responsible for enforcing the system's rules and maintaining order. Executes tasks assigned by the Architect and operates with unwavering determination to eliminate threats to the Matrix, particularly targeting rogue elements such as Neo and his allies."
+  },
+  {
+    "jobTitle": "Field Operative",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Various Locations within the Matrix",
+    "description":
+        "As a Field Operative, tasked with hunting down and neutralizing human rebels who pose a threat to the stability of the Matrix. Proficient in combat tactics, surveillance, and infiltration techniques. Demonstrates exceptional adaptability and resourcefulness in high-pressure situations."
+  },
+  {
+    "jobTitle": "Surveillance Specialist",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Virtual Reality",
+    "description":
+        "Specializes in surveillance operations within the Matrix, monitoring key individuals and activities to maintain the system's control. Utilizes advanced tracking abilities and cutting-edge technology to gather intelligence and identify potential threats. Collaborates closely with other agents to coordinate strategic interventions."
+  },
+  {
+    "jobTitle": "Interrogation Expert",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Interrogation Facility",
+    "description":
+        "Serves as the primary interrogation expert within the Matrix, extracting valuable information from captured rebels and potential threats to the system. Implements psychological manipulation techniques and advanced interrogation methods to break resistance and extract crucial intelligence. Plays a pivotal role in uncovering rebel hideouts and disrupting resistance movements."
+  },
+  {
+    "jobTitle": "Senior Agent",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Virtual Reality",
+    "description":
+        "As a Senior Agent within the Matrix, responsible for enforcing the system's rules and maintaining order. Executes tasks assigned by the Architect and operates with unwavering determination to eliminate threats to the Matrix, particularly targeting rogue elements such as Neo and his allies."
+  },
+  {
+    "jobTitle": "Field Operative",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Various Locations within the Matrix",
+    "description":
+        "As a Field Operative, tasked with hunting down and neutralizing human rebels who pose a threat to the stability of the Matrix. Proficient in combat tactics, surveillance, and infiltration techniques. Demonstrates exceptional adaptability and resourcefulness in high-pressure situations."
+  },
+  {
+    "jobTitle": "Surveillance Specialist",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Virtual Reality",
+    "description":
+        "Specializes in surveillance operations within the Matrix, monitoring key individuals and activities to maintain the system's control. Utilizes advanced tracking abilities and cutting-edge technology to gather intelligence and identify potential threats. Collaborates closely with other agents to coordinate strategic interventions."
+  },
+  {
+    "jobTitle": "Interrogation Expert",
+    "company": "The Matrix",
+    "dates": "1999 - Present",
+    "location": "Interrogation Facility",
+    "description":
+        "Serves as the primary interrogation expert within the Matrix, extracting valuable information from captured rebels and potential threats to the system. Implements psychological manipulation techniques and advanced interrogation methods to break resistance and extract crucial intelligence. Plays a pivotal role in uncovering rebel hideouts and disrupting resistance movements."
+  },
+
+];
