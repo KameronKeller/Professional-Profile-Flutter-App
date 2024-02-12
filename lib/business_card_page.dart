211f9c5c47c398c7ec62638106da5e271f90437f
Kameron Keller <mrkameronkeller@gmail.com>
2024-02-12 11:34:15 -0800

    Add ability to open links and display photo

--- lib/business_card_page.dart
+++ lib/business_card_page.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:professional_profile_app/centered_row.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BusinessCardPage extends StatelessWidget {
   const BusinessCardPage({super.key});
+  final String phoneNumber = "555-555-5555";
+
+  // void sendSms() {
+  //   _launchAppWithUrl(
+  //     scheme: 'sms',
+  //     path: phoneNumber,
+  //   );
+  // }
+
+  // void openPortfolio() {
+  //   _launchAppWithUrl(
+  //     scheme: 'sms',
+  //     path: phoneNumber,
+  //   );
+  // }
+
+
+
+  Future<void> _launchAppWithUrl({required String scheme, required String path}) async {
+    final Uri launchUri = Uri(
+      scheme: scheme,
+      path: path,
+    );
+    await launchUrl(launchUri);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.all(25.0),
       child: Column(
         children: [
-          CenteredRow(children: [SizedBox(width: 200, height: 200, child: Placeholder())],),
-          CenteredRow(
+          const CenteredRow(children: [SizedBox(width: 200, height: 200, child: Image(
+            image: AssetImage('assets/agent_smith.jpg')
+          ))],),
+          const CenteredRow(
             children: [
               Text(
-                "Mr. Anderson",
+                "Agent Smith",
                 style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
@@ -22,35 +50,62 @@ class BusinessCardPage extends StatelessWidget {
               ),
             ],
           ),
-          CenteredRow(children: [
+          const CenteredRow(children: [
             Text(
-              "Full Stack Engineer",
-              style: TextStyle(
-                fontSize: 18,
-              ),
-            )
-          ]),
-          CenteredRow(children: [
-            Text(
-              "Phone Number",
+              "Intelligent AI Program",
               style: TextStyle(
                 fontSize: 18,
               ),
             )
           ]),
           CenteredRow(
+            children: [
+              TextButton(
+                onPressed: () {
+                  _launchAppWithUrl(
+                    path: phoneNumber,
+                    scheme: 'sms',
+                  );
+                },
+                child: const Text(
+                  "110-0101-1111",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                )
+              ),
+            ]),
+          CenteredRow(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-            Text(
-              "github.com/Rudxain/RGB-digital-rain",
-              style: TextStyle(
-                fontSize: 18,
+            TextButton(
+                onPressed: () {
+                  _launchAppWithUrl(
+                    path: 'github.com/Rudxain/RGB-digital-rain',
+                    scheme: 'https',
+                  );
+                },
+                child: const Text(
+                  "github.com/agent_smith",
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                )
               ),
-            ),
-            // SizedBox(width: 10.0),
-            Text(
-              "Email",
-              style: TextStyle(fontSize: 18),)
+            TextButton(
+                onPressed: () {
+                  _launchAppWithUrl(
+                    path: "smith@matrix.com",
+                    scheme: 'mailto',
+                  );
+                },
+                child: const Text(
+                  "smith@matrix.com",
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                )
+              ),
           ]),
         ],
       ),
