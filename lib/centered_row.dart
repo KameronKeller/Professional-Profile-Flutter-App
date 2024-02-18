3b5e11da360b8d931aa52a8897c22563b9ed82e6
Kameron Keller <mrkameronkeller@gmail.com>
2024-02-18 15:36:54 -0800

    Change default padding on centered row

--- lib/centered_row.dart
+++ lib/centered_row.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CenteredRow extends StatelessWidget {
-
   final List<Widget> children;
   final double padding;
   final MainAxisAlignment mainAxisAlignment;
 
-  const CenteredRow({super.key, required this.children, this.padding = 5.0, this.mainAxisAlignment = MainAxisAlignment.center});
+  const CenteredRow(
+      {super.key,
+      required this.children,
+      this.padding = 0.0,
+      this.mainAxisAlignment = MainAxisAlignment.center});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(padding),
-      child: Row(
-        mainAxisAlignment: mainAxisAlignment,
-        children: children,
-      ),
+    return Row(
+      mainAxisAlignment: mainAxisAlignment,
+      children: children,
     );
   }
 }
