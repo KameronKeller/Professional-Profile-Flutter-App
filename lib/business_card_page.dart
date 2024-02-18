26863a924d99b67eeaf725cb24223601e4ac8620
Kameron Keller <mrkameronkeller@gmail.com>
2024-02-18 15:06:00 -0800

    Create business card widgets in the constructor

--- lib/business_card_page.dart
+++ lib/business_card_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:professional_profile_app/centered_row.dart';
 import 'package:professional_profile_app/main.dart';
+import 'package:professional_profile_app/widgets/contact_method.dart';
+import 'package:professional_profile_app/widgets/current_position.dart';
+import 'package:professional_profile_app/widgets/profile_name.dart';
+import 'package:professional_profile_app/widgets/profile_photo.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class BusinessCardPage extends StatelessWidget {
@@ -19,176 +23,108 @@ class BusinessCardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(25.0),
-      child: Column(
-        children: [
-          ProfilePhoto(userProfile: userProfile),
-          ProfileName(userProfile: userProfile),
-          CurrentPosition(userProfile: userProfile),
-          ContactMethod(
-            path: userProfile.phoneNumber,
-            scheme: 'sms',
-            textLabel: userProfile.phoneNumber,
-            urlLauncher: _launchAppWithUrl,
-            fontSize: 18,
-          ),
-          Row(children: [
-            Column(children: [
-              ContactMethod(
-                path: 'github.com/Rudxain/RGB-digital-rain',
-                scheme: 'https',
-                textLabel: userProfile.github,
-                urlLauncher: _launchAppWithUrl,
-                fontSize: 13,
-              ),
-            ],),
-              Column(
-                children: [
-                  ContactMethod(
-                    path: userProfile.email,
-                    scheme: 'mailto',
-                    textLabel: userProfile.email,
-                    urlLauncher: _launchAppWithUrl,
-                    fontSize: 13,
-                  ),
-                ],
-              ),
-          ],)
-        ],
-      ),
+    return ResponsiveBusinessCard(userProfile: userProfile);
+  }
+}
+
+class ResponsiveBusinessCard extends StatelessWidget {
+  ResponsiveBusinessCard({super.key, required this.userProfile}) {
+    profilePhoto = ProfilePhoto(userProfile: userProfile);
+    profileName = ProfileName(userProfile: userProfile);
+    currentPosition = CurrentPosition(userProfile: userProfile);
+    phoneContact = ContactMethod(
+      path: userProfile.phoneNumber,
+      scheme: 'sms',
+      textLabel: userProfile.phoneNumber,
+      urlLauncher: _launchAppWithUrl,
+      fontSize: 18,
+    );
+    github = ContactMethod(
+      path: 'github.com/Rudxain/RGB-digital-rain',
+      scheme: 'https',
+      textLabel: userProfile.github,
+      urlLauncher: _launchAppWithUrl,
+      fontSize: 13,
+    );
+    email = ContactMethod(
+      path: userProfile.email,
+      scheme: 'mailto',
+      textLabel: userProfile.email,
+      urlLauncher: _launchAppWithUrl,
+      fontSize: 13,
     );
   }
-}
-
-class CurrentPosition extends StatelessWidget {
-  const CurrentPosition({
-    super.key,
-    required this.userProfile,
-  });
 
   final UserProfile userProfile;
+  late final ProfilePhoto profilePhoto;
+  late final ProfileName profileName;
+  late final CurrentPosition currentPosition;
+  late final ContactMethod phoneContact;
+  late final ContactMethod github;
+  late final ContactMethod email;
 
-  @override
-  Widget build(BuildContext context) {
-    return CenteredRow(children: [
-      Text(
-        userProfile.currentPosition,
-        style: const TextStyle(
-          fontSize: 18,
-        ),
-      )
-    ]);
-  }
-}
 
-class ProfileName extends StatelessWidget {
-  const ProfileName({
-    super.key,
-    required this.userProfile,
-  });
-
-  final UserProfile userProfile;
-
-  @override
-  Widget build(BuildContext context) {
-    return CenteredRow(
-      children: [
-        Text(
-          userProfile.name,
-          style: const TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ],
+  Future<void> _launchAppWithUrl(
+      {required String scheme, required String path}) async {
+    final Uri launchUri = Uri(
+      scheme: scheme,
+      path: path,
     );
+    await launchUrl(launchUri);
   }
-}
 
-class ProfilePhoto extends StatelessWidget {
-  const ProfilePhoto({
-    super.key,
-    required this.userProfile,
-  });
-
-  final UserProfile userProfile;
+  
 
   @override
   Widget build(BuildContext context) {
-    return CenteredRow(children: [SizedBox(width: 200, height: 200, child: Image(
-      image: AssetImage(userProfile.profilePhotoPath)
-    ))],);
-  }
-}
-
-
-// class PhoneNumber extends StatelessWidget {
-//   const PhoneNumber({
-//     super.key,
-//     required this.userProfile,
-//     required this.urlLauncher,
-//   });
-  
-//   final UserProfile userProfile;
-//   final Future<void> Function({required String path, required String scheme}) urlLauncher;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return CenteredRow(
-//             children: [
-//               TextButton(
-//                 onPressed: () {
-//                   urlLauncher(
-//                     path: userProfile.phoneNumber,
-//                     scheme: 'sms',
-//                   );
-//                 },
-//                 child: Text(
-//                   userProfile.phoneNumber,
-//                   style: const TextStyle(
-//                     fontSize: 18,
-//                   ),
-//                 )
-//               ),
-//             ]);
-//   }
-// }
-
-class ContactMethod extends StatelessWidget {
-  const ContactMethod({
-    super.key,
-    required this.path,
-    required this.scheme,
-    required this.textLabel,
-    required this.urlLauncher,
-    required this.fontSize,
-  });
-  
-  final String path;
-  final String scheme;
-  final String textLabel;
-  final Future<void> Function({required String path, required String scheme}) urlLauncher;
-  final double fontSize;
-
-  @override
-  Widget build(BuildContext context) {
-    return CenteredRow(
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        return Padding(
+          padding: EdgeInsets.all(25.0),
+          child: Column(
             children: [
-              TextButton(
-                onPressed: () {
-                  urlLauncher(
-                    path: path,
-                    scheme: scheme,
-                  );
-                },
-                child: Text(
-                  textLabel,
-                  style: TextStyle(
-                    fontSize: fontSize,
+              profilePhoto,
+              profileName,
+              currentPosition,
+              phoneContact,
+              // ProfileName(userProfile: userProfile),
+              // CurrentPosition(userProfile: userProfile),
+              // ContactMethod(
+              //   path: userProfile.phoneNumber,
+              //   scheme: 'sms',
+              //   textLabel: userProfile.phoneNumber,
+              //   urlLauncher: _launchAppWithUrl,
+              //   fontSize: 18,
+              // ),
+              Row(children: [
+                Column(children: [
+                  github,
+                  
+                  // ContactMethod(
+                  //   path: 'github.com/Rudxain/RGB-digital-rain',
+                  //   scheme: 'https',
+                  //   textLabel: userProfile.github,
+                  //   urlLauncher: _launchAppWithUrl,
+                  //   fontSize: 13,
+                  // ),
+                ],),
+                  Column(
+                    children: [
+                      email,
+                      // ContactMethod(
+                      //   path: userProfile.email,
+                      //   scheme: 'mailto',
+                      //   textLabel: userProfile.email,
+                      //   urlLauncher: _launchAppWithUrl,
+                      //   fontSize: 13,
+                      // ),
+                    ],
                   ),
-                )
-              ),
-            ]);
+              ],)
+            ],
+          ),
+        );
+      }
+    );
   }
 }
